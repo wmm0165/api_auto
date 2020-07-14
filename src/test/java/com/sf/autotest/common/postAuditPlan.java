f5d854fbf933178eaa18dbe49a7ae6c10b599305
wangmengmeng <wmm_0165@163.com>
2020-07-15 00:25:36 +0800

    auditPlan

--- src/test/java/com/sf/autotest/common/postAuditPlan.java
+++ src/test/java/com/sf/autotest/common/postAuditPlan.java
@@ -1,7 +1,12 @@
 package com.sf.autotest.common;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description
@@ -11,32 +16,43 @@ import com.alibaba.fastjson.JSONObject;
 public class postAuditPlan {
     private HttpRequest httpRequest = new HttpRequest();
 
-    public void postAuditPlan(){
+
+    public void postAuditPlan() {
         JSONObject plan = new JSONObject();
-        plan.put("name","住院");
-        plan.put("category","");
-        plan.put("createdTime","");
-        plan.put("modifiedTime","");
-        plan.put("recipeSource","");
-        plan.put("deptList","");
-        plan.put("groupList","");
-        plan.put("isOuvas","");
-        plan.put("isPivas","");
-        plan.put("minStay","");
-        plan.put("maxStay","");
-        plan.put("minAge","");
-        plan.put("maxAge","");
-        plan.put("ageUnit","");
-        plan.put("costTypes","");
-        plan.put("patientCondition","");
-        plan.put("iptWardList","");
-        plan.put("weekList","");
-        plan.put("doctorList","");
-        plan.put("startTime","");
-        plan.put("endTime","");
+        JSONArray deptList = new JSONArray();
+        JSONArray groupList = new JSONArray();
+        JSONArray iptWardList = new JSONArray();
+        ArrayList<Object> weekList = new ArrayList<>();
+        JSONArray doctorList = new JSONArray();
+        plan.put("name", "住院");
+        plan.put("category", 2);
+        plan.put("createdTime", 1594739619244L);
+        plan.put("modifiedTime", 1594739619244L);
+        plan.put("recipeSource", "0");
+        plan.put("deptList", deptList);
+        plan.put("groupList", groupList);
+        plan.put("isOuvas", 0);
+        plan.put("isPivas", 0);
+        plan.put("minStay", "");
+        plan.put("maxStay", "");
+        plan.put("minAge", "");
+        plan.put("maxAge", "");
+        plan.put("ageUnit", "岁");
+        plan.put("costTypes", "");
+        plan.put("patientCondition", "");
+        plan.put("iptWardList", iptWardList);
+        plan.put("weekList", weekList);
+        plan.put("doctorList", doctorList);
+        plan.put("startTime", "00:00");
+        plan.put("endTime", "23:59");
+        JSONObject res = httpRequest.doPostJson(sfContant.auditcenter_url + "/api/v1/auditPlan", plan);
+        System.out.println(res);
 
+    }
 
-
+    public static void main(String[] args) {
+        postAuditPlan auditPlan = new postAuditPlan();
+        auditPlan.postAuditPlan();
     }
 
 }
